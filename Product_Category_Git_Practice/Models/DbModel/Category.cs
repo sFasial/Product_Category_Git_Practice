3441751d0cd37c2f4dbf50668b3b22b286c56c38
faisal shaikh <sfaisal3004@gmail.com>
2023-08-01 11:13:30 +0530

    Added Db Models Of Product & Category

---
+++ Product_Category_Git_Practice/Models/DbModel/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product_Category_Git_Practice.Models.DbModel
+{
+    [Table("Categories_Git")]
+    public class Category
+    {
+        [Key]
+        public int category_id { get; set; }
+        public string category_name { get; set; }
+        public string category_description { get; set; }
+    }
+}
